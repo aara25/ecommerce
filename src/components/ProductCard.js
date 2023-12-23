b3f5b9c1debb6efdd4c0e4fd36f28ac1e82d4bdd
aara25 <aarathisekar2505@gmail.com>
2023-12-23 20:17:43 +0530

    cart started

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/reducer/cart";
 
 export default function ProductCard(props) {
+    const dispatch = useDispatch();
+    const addToCart = () => {
+        dispatch(addItem(props))
+    };
   return (
     <div className="card m-2" style={{ width: 300 }}>
       <div className="mt-2">
@@ -16,7 +22,7 @@ export default function ProductCard(props) {
       <h6 className="mt-2">Price : {`Rs ${props.price}`}</h6>
       <h6 className="mt-2">Description : {props.description}</h6>
       <div>
-        <button className="btn btn-success m-2">Add to Cart</button>
+        <button className="btn btn-success m-2" onClick={addToCart}>Add to Cart</button>
       </div>
     </div>
   );
