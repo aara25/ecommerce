18ecde3513e1fe4b2de2eb32cac932885f8c95cb
aara25 <aarathisekar2505@gmail.com>
2023-12-24 11:33:52 +0530

    ecommerce website added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
-    <div className='p-3 bg-secondary bg-gradient'>
-        <h3>E-Commerce Website</h3>
-        <div className='row justify-content-center pt-2 pb-1'>
-            <div className='col-sm-12 col-md-7 col-lg-6 col-xl-5 d-flex'>
-                <button className='btn btn-success me-4'>Home</button>
-                <input className='form-control' type='search' placeholder='Search...'></input>
-                <button className='btn btn-success ms-3'>Cart</button>
-            </div>
+    <div className='p-3' style={{ background: 'linear-gradient(45deg, #FF6B6B, #FF8E53)' }}>
+      <h3 style={{ color: '#fff' }}>E-Commerce Website</h3>
+      <div className='row justify-content-center pt-2 pb-1'>
+        <div className='col-sm-12 col-md-7 col-lg-6 col-xl-5 d-flex'>
+          <button className='btn btn-success me-4' onClick={() => navigate('/ecommerce')}>
+            Home
+          </button>
+          <input className='form-control' type='search' placeholder='Search...'></input>
+          <button className='btn btn-success ms-3' onClick={() => navigate('/ecommerce/cart')}>
+            Cart
+          </button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/container/Home.js
+++ src/container/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Header from '../components/Header'
 import Dashboard from './Dashboard'
+import Cart from './Cart'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <Header></Header>
         <Routes>
             <Route path="/ecommerce" element={<Dashboard />} />
+            <Route path="/ecommerce/cart" element={<Cart />} />
         </Routes>
     </div>
   )
